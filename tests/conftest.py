81d3170073d5d34a5b876fbe92aeac9c7eb928cd
Ori Kronfeld <ori.kronfeld@weizmann.ac.il>
2024-12-09 15:19:48 +0200

    adding cuda test

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import shutil
+import pytest
+
+def pytest_addoption(parser):
+    """Docstring for pytest_addoption."""
+    parser.addoption(
+        "--accelerator",
+        action="store",
+        default="cpu",
+        help="Option to specify which accelerator to use for tests.",
+    )
+    parser.addoption(
+        "--devices",
+        action="store",
+        default="auto",
+        help="Option to specify which devices to use for tests.",
+    )
+
+@pytest.fixture(scope="session")
+def accelerator(request):
+    """Docstring for accelerator."""
+    return request.config.getoption("--accelerator")
+
+
+@pytest.fixture(scope="session")
+def devices(request):
+    """Docstring for devices."""
+    return request.config.getoption("--devices")
